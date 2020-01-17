8cac4d8b8310e7c6f14b41d06a9aa774f82ea579
Flavjack <flavjack@gmail.com>
2020-01-17 23:40:13 +0100

    typo

--- cnfg/setup.r
+++ cnfg/setup.r
@@ -1,26 +1,49 @@
+# -------------------------------------------------------------------------
+# Knitr options -----------------------------------------------------------
+# -------------------------------------------------------------------------
 
 knitr::opts_chunk$set(
   fig.align = "center", # Center images in the export file
-  out.width = "98%",
+  out.width = "98%", # Figure width in htlm
 	echo = FALSE, # Avoid print code in the export file
   message = FALSE,  # Avoid print messages in the export file
   warning = FALSE # Avoid print messages in the export file
   )
 
+# -------------------------------------------------------------------------
+# Compile options ---------------------------------------------------------
+# -------------------------------------------------------------------------
+
 options(
   digits = 4, # Number of digits in the values
   OutDec= ".", # Use "." insted of "," in the decimal values
   scipen = 99, # Avoid use "6e-04"
-  knitr.kable.NA = '', # NA values will appear as empty cell
+  knitr.kable.NA = "", # NA values will appear as empty cell
   knitr.table.format = "pandoc", # Format for export tables
   citr.use_betterbiblatex = FALSE, # For zotero addin 
   gargle_oob_default = TRUE # Googlesheets4 auth process
   ) 
 
+# -------------------------------------------------------------------------
+# Folder options ----------------------------------------------------------
+# -------------------------------------------------------------------------
+
+if(!dir.exists("files"))
+  {dir.create("files")}
+if(exists("fb"))
+  {openxlsx::write.xlsx(fb,"files/data.xlsx")}
+if(dir.exists("docs"))
+  {zip::zipr(zipfile = "docs/files.zip", files = "files")}
+
+# -------------------------------------------------------------------------
+# Packages ----------------------------------------------------------------
+# -------------------------------------------------------------------------
+
 library(devtools) # For install packages and complements from dev sources
 library(bookdown) # Required for use the present template
 library(citr) # Use Zotero for citations
 library(knitr) # Base of the bookdown template
+library(kableExtra) # Table notes!
 library(inserttable) # Insert table with copy and paste
 library(tidyverse) # Data manipulation
 library(googlesheets4) # Read/write google sheets files
@@ -33,6 +56,4 @@ library(grid) # For mergue figures
 library(png) # Import png files
 library(jpeg) # Import jpeg files
 
-
-
-
+knitr::write_bib(c(.packages()),'cnfg/pkgs.bib')
